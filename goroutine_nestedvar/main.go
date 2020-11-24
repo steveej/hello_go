013dcdde8c324113126dea9b8158f9b6403cca4a
Stefan Junker <mail@stefanjunker.de>
2020-11-24 10:45:34 +0100

    catch up

--- goroutine_nestedvar/main.go
+++ goroutine_nestedvar/main.go
@@ -12,34 +12,33 @@ type Request struct {
 	n      *int
 }
 
-func serve(queue chan *Request) {
+func serve(queue chan Request) {
 	var (
 		wgInner sync.WaitGroup
 	)
 	for req := range queue {
 		wgInner.Add(1)
 		realreq := req
-		go func() {
+		f := func() {
 			myn := &req.n
 			realn := **myn
 
 			wgInner.Done()
 			time.Sleep(time.Duration(req.sleep) * time.Second)
 			fmt.Println(req.number, realreq.number, myn, *myn, **myn, &realn, realn)
-		}()
+		}
+		go f()
 		wgInner.Wait()
 	}
 }
 
 func main() {
-	queue := make(chan *Request)
+	queue := make(chan Request)
 	go serve(queue)
 
-	n := 1
-	queue <- &Request{n, n, &n}
-
-	n = 0
-	queue <- &Request{n, n, &n}
-
-	time.Sleep(2 * time.Second)
+	max := 10
+	for n := 0; n < max; n++ {
+		queue <- Request{n, n, &n}
+	}
+	time.Sleep(time.Duration(max) * time.Second)
 }
